f972d7004b8a6df929bad8ff0a0fda34d50dce03
shashank <shshnk28@gmail.com>
2016-01-28 14:29:31 +0530

    new branch to take input events from a file

--- src/mainpkg/DriverClass.java
+++ src/mainpkg/DriverClass.java
@@ -16,23 +16,41 @@ import org.slf4j.LoggerFactory;
 import helperClasses.ExecutionPlan;
 import helperClasses.FileAnalysis;
 
-public class DriverClass {
+public class DriverClass implements iSyntheticEventGen{
+	static String pathToFile = null;
+	EventGen eventGen;
 	String dataSetType = "PLUG-11";
 	InputHandler inputHandler = null;
 	File outFile  = null;
 	HashMap<Long, Integer> inputHashMap = null;
 	HashMap<Long, Integer> outputHashMap = null;
+	private static int numThreads= 0;
 	public static String queryType = null;
 	public static Logger Log = LoggerFactory.getLogger(DriverClass.class);
 	public static void main(String[] args) {
-		// initialise the file path here 
-		Log.info("experiment started");
-		queryType = args[0];		
+		Log.info("experiment started");		
+		if (args.length != 3) {
+			System.out.println("Please enter the filname ,query type, number of threads as parameters.. Returning");
+			return;
+		}
+		pathToFile = args[0];
+		File testFile = new File(pathToFile);
+		if(testFile.exists() == false || testFile.isDirectory() == true) {
+			System.out.println("file does not exist.. Returning");
+			return;
+		}
+		queryType = args[1];
+	
+		GlobalConstants.numThreads = Integer.parseInt(args[2]);
+		if (GlobalConstants.numThreads < 1 ||  GlobalConstants.numThreads > 4) {
+			System.out.println("Proj can run only one 1,2,3 or 4 threads.. Returning");
+			return;
+		}
+		
 		DriverClass dc = new DriverClass();
 		Boolean val =dc.initiateExecutionPlan();
 		if (val == true) {
 			dc.initiateEventGen();
-			dc.sendDatatoSiddhi();
 		}
 		
 	}
@@ -52,11 +70,16 @@ public class DriverClass {
             @Override
             public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
 //                EventPrinter.print(inEvents);
+            	// write the data in outfile 
+//                long ts = inEvents[0].getTimestamp();
+//                int data = inEvents[0].getData();
+                
                 for (Event eve:inEvents) {
                 	Long key = Long.valueOf(System.currentTimeMillis()/1000);
         			if(outputHashMap.containsKey(key)) {
         				Integer tempInt = outputHashMap.get(key);
         				outputHashMap.put(key, tempInt+1);
+        				
         			}
         			else {
         				outputHashMap.put(Long.valueOf(System.currentTimeMillis()/1000), Integer.valueOf(1));
@@ -72,69 +95,29 @@ public class DriverClass {
 	private void initiateEventGen () {
 		inputHashMap=  new HashMap<Long,Integer>();
 		outputHashMap=  new HashMap<Long,Integer>();
+		eventGen=new EventGen(this,1); // scaling factor is 1
+		try {
+            eventGen.launch(pathToFile,dataSetType);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw  new RuntimeException(e);
+        }
 	}
-	
-	public void sendDatatoSiddhi()  {	
-		Long curentTime = System.currentTimeMillis()/1000;
-		Long key = curentTime; 
-		/*
-		Object[] obj1 = {10};
-		Object[] obj2 = {5};
-//		Object[] obj1 = {Integer.parseInt("10")};
-//		Object[] obj2 = {Integer.parseInt("5")};
-		for (int i=0;i<10;i++) {
-			try {
-				inputHandler.send(obj1);
-//				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		for (int i=0;i<20;i++) {
-			try {
-				inputHandler.send(obj2);
-//				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		Log.info("DataSet over");
-		*/
-//		Object[] obj = {Integer.parseInt("1")};
-//		Object[] obj = {1};
-		
-		 Object[] obj= new Object[1];
-		int[] objArr = {2,2,2};
-		int counter = 0;
-		while(curentTime+30 > key)  {
-			if(counter >= 3) {
-				counter = 0;
-			}
-			obj[0] = objArr[counter] ;
-			counter++;
-			try {
-				inputHandler.send(obj);
-//				Thread.sleep(1);
-//				System.out.println(obj[0]);
-//				hashMapupdate();
-				key = Long.valueOf(System.currentTimeMillis()/1000);
-				if(inputHashMap.containsKey(key)) {
-					Integer tempInt = inputHashMap.get(key);
-					inputHashMap.put(key, tempInt+1);
-				}
-				else {
-					inputHashMap.put(Long.valueOf(System.currentTimeMillis()/1000), Integer.valueOf(1));
-				}
-//				Thread.sleep(1);
-			} catch (InterruptedException ie) {
-				System.out.println("could not send to Siddhi");
-				ie.printStackTrace();
-			} 	
+	@Override
+    public void receive(List<String> event)  {	 
+		try {
+			inputHandler.send(new Object[]{Integer.parseInt(event.get(2))});
+			hashMapupdate();
+			
+		} catch (InterruptedException ie) {
+			System.out.println("could not send to Siddhi");
+			ie.printStackTrace();
 		} 
+//		catch (IOException ex) {
+//			System.out.println("could not write to input file");
+//			ex.printStackTrace();
+//		}
 		
-		fileWrite();
 	}
 	private synchronized void hashMapupdate () {
 		Long key = Long.valueOf(System.currentTimeMillis()/1000);
@@ -146,13 +129,36 @@ public class DriverClass {
 			inputHashMap.put(Long.valueOf(System.currentTimeMillis()/1000), Integer.valueOf(1));
 		}
 	}
-	
+	@Override
+	public void dataSetOver() {
+		Log.info("datasetOver called");
+//		for (Map.Entry<Long, Integer> entry: inputHashMap.entrySet()) {
+//			Long key  = entry.getKey();
+//			Integer val = entry.getValue();
+//			System.out.println("key val pair\n");
+//			System.out.println(key);
+//			System.out.println(val);
+//		}
+//			inputFileBuff.close();
+		try {
+			Thread.sleep(10000);
+			fileWrite();
+		} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
 	private synchronized void fileWrite () {
-		Log.info("DataSet over fileWrite called");
+		numThreads++;
+		if (numThreads !=GlobalConstants.numThreads) {
+			System.out.println("sskipping file write call");
+			return;
+		}
+		Log.info("fileWrite called");
 		FileAnalysis obj = new FileAnalysis();
 //		obj.calculateFrequency(inputFile.getAbsolutePath(), "result-"+pathToFile);
-		obj.calculateFrequencyFromBuffer(inputHashMap, "inputResult.csv");
-		obj.calculateFrequencyFromBuffer(outputHashMap, "outputResult.csv");
+		obj.calculateFrequencyFromBuffer(inputHashMap, "inputRes-" + pathToFile);
+		obj.calculateFrequencyFromBuffer(outputHashMap, "outputRes-" + pathToFile);
 //		obj.calculateFrequency(outFile.getAbsolutePath(), "result-"+pathToFile);
 		Log .info("experiment over");
 	}
