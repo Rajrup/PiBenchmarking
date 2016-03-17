116c7b4c10d90941ee60be52c1d3dbfe7b5a485c
shashank <shshnk28@gmail.com>
2016-03-17 15:55:24 +0530

    set up for the evergy monitoring code

--- src/mainpkg/DriverClass.java
+++ src/mainpkg/DriverClass.java
@@ -1,6 +1,8 @@
 package mainpkg;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import org.wso2.siddhi.core.ExecutionPlanRuntime;
 import org.wso2.siddhi.core.SiddhiManager;
@@ -12,26 +14,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import helperClasses.ExecutionPlan;
-import helperClasses.FileAnalysis;
 
 public class DriverClass {
-	String dataSetType = "PLUG-11";
+	/*
+	 * constants
+	 */
+	private static final int SHORT_CONSTANT_INPUT = 1;
+	private static final int SHORT_VARIED_INPUT = 2;
+	private static final int LONG_CONSTANT_INPUT = 3;
+	private static final int LONG_VARIED_INPUT = 4;
+	private static final int SEQ_INPUT = 5;
+	private static final int FILE_WRITE_INTERVAL = 60;
+	private static final String fileName = "outputfile.txt";
 	InputHandler inputHandler = null;
 	File outFile = null;
 	HashMap<Long, Integer> inputHashMap = null;
 	HashMap<Long, Integer> outputHashMap = null;
 	private final Integer one = Integer.valueOf(1);
 	public static String queryType = null;
-	private static final int SHORT_CONSTANT_INPUT = 1;
-	private static final int SHORT_VARIED_INPUT = 2;
-	private static final int LONG_CONSTANT_INPUT = 3;
-	private static final int LONG_VARIED_INPUT = 4;
-	private static final int SEQ_INPUT = 5;
+	
 	private static int input_type;
+	Long eventsSent;
+	Long eventsReceived;
 	public static Logger Log = LoggerFactory.getLogger(DriverClass.class);
 
 	public static void main(String[] args) {
-		// initialise the file path here
+		// initialize the file path here
 		Log.info("experiment started");
 		queryType = args[0];
 		input_type = Integer.valueOf(args[1]);
@@ -58,6 +66,7 @@ public class DriverClass {
 		executionPlanRuntime.addCallback("query1", new QueryCallback() {
 			@Override
 			public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+				eventsReceived++;
 				if (input_type == SHORT_CONSTANT_INPUT || input_type == SHORT_VARIED_INPUT) {
 					EventPrinter.print(inEvents);
 					return;
@@ -86,7 +95,8 @@ public class DriverClass {
 	}
 
 	public void sendDatatoSiddhi() {
-
+		eventsSent = 0l;
+		eventsReceived = 0l;
 		switch (input_type) {
 		case SHORT_CONSTANT_INPUT: {
 			System.out.println("input thread");
@@ -94,6 +104,7 @@ public class DriverClass {
 			// Object[] obj1 = {Integer.parseInt("10")};
 			// Object[] obj2 = {Integer.parseInt("5")};
 			for (int i = 0; i < 5; i++) {
+				eventsSent++;
 				Object[] obj1 = { 10 };
 				try {
 					inputHandler.send(obj1);
@@ -104,6 +115,7 @@ public class DriverClass {
 				}
 			}
 			for (int i = 0; i < 10; i++) {
+				eventsSent++;
 				Object[] obj2 = { 10 };
 				try {
 					inputHandler.send(obj2);
@@ -119,6 +131,7 @@ public class DriverClass {
 			int[] objArr = {3,7,9,11};
 			int counter = 0;
 			for (int i = 0; i < 20; i++) {
+				eventsSent++;
 				if (counter >= 4) {
 					counter = 0;
 				}
@@ -137,34 +150,30 @@ public class DriverClass {
 			}
 			break;
 		}
-		case LONG_CONSTANT_INPUT:
-			Long curentTime = System.currentTimeMillis() / 1000;
-			Long key = curentTime;
-			while (curentTime + 30 > key) {
+		case LONG_CONSTANT_INPUT: {
+			Long startTime = System.currentTimeMillis() / 1000;
+			while (true) {
 				try {
 					Object[] obj = { 10 };
+					eventsSent++;
 					inputHandler.send(obj);
-					key = Long.valueOf(System.currentTimeMillis() / 1000);
-					Integer tempInt = inputHashMap.get(key);
-					if (tempInt != null) {
-						inputHashMap.put(key, tempInt + 1);
-					} else {
-						inputHashMap.put(key, one);
+					Long currentTime =System.currentTimeMillis() / 1000;
+					if(currentTime - startTime >= FILE_WRITE_INTERVAL){
+						startTime  = currentTime;
+						writeDataToFile(currentTime);
 					}
 				} catch (InterruptedException ie) {
 					System.out.println("could not send to Siddhi");
 					ie.printStackTrace();
 				}
 			}
-
-			fileWrite();
-			break;
+			
+		}
 		case LONG_VARIED_INPUT: {
-			curentTime = System.currentTimeMillis() / 1000;
-			key = curentTime;
+			Long startTime = System.currentTimeMillis() / 1000;
 			int[] objArr = {3,7,9};
 			int counter = 0;
-			while (curentTime + 30 > key) {
+			while (true) {
 				if (counter >= 3) {
 					counter = 0;
 				}
@@ -173,36 +182,26 @@ public class DriverClass {
 				counter++;
 				try {
 					inputHandler.send(obj);
-					// Thread.sleep(1);
-					// System.out.println(obj[0]);
-					// hashMapupdate();
-					key = Long.valueOf(System.currentTimeMillis() / 1000);
-
-					Integer tempInt = inputHashMap.get(key);
-					if (tempInt != null) {
-						inputHashMap.put(key, tempInt + 1);
-					} else {
-						inputHashMap.put(key, one);
+					Long currentTime = Long.valueOf(System.currentTimeMillis() / 1000);
+					if(currentTime - startTime >= FILE_WRITE_INTERVAL){
+						startTime  = currentTime;
+						writeDataToFile(currentTime);
 					}
-					// Thread.sleep(1);
 				} catch (InterruptedException ie) {
 					System.out.println("could not send to Siddhi");
 					ie.printStackTrace();
 				}
 			}
-
-			fileWrite();
-			break;
+			
 		}
 		/*
 		 * this is a special input type to test the sequence query
 		 */
 		case SEQ_INPUT: {
-			curentTime = System.currentTimeMillis() / 1000;
-			key = curentTime;
+			Long startTime = System.currentTimeMillis() / 1000;
 			int[] objArr = {3,3,9};
 			int counter = 0;
-			while (curentTime + 30 > key) {
+			while (true) {
 				if (counter >= 3) {
 					counter = 0;
 				}
@@ -211,16 +210,10 @@ public class DriverClass {
 				counter++;
 				try {
 					inputHandler.send(obj);
-					// Thread.sleep(1);
-					// System.out.println(obj[0]);
-					// hashMapupdate();
-					key = Long.valueOf(System.currentTimeMillis() / 1000);
-
-					Integer tempInt = inputHashMap.get(key);
-					if (tempInt != null) {
-						inputHashMap.put(key, tempInt + 1);
-					} else {
-						inputHashMap.put(key, one);
+					Long currentTime = Long.valueOf(System.currentTimeMillis() / 1000);
+					if(currentTime - startTime >= FILE_WRITE_INTERVAL){
+						startTime  = currentTime;
+						writeDataToFile(currentTime);
 					}
 					// Thread.sleep(1);
 				} catch (InterruptedException ie) {
@@ -228,33 +221,19 @@ public class DriverClass {
 					ie.printStackTrace();
 				}
 			}
-
-			fileWrite();
-			break;
 		}
 		}
 
 	}
-	private synchronized void hashMapupdate() {
-		Long key = Long.valueOf(System.currentTimeMillis() / 1000);
-		if (inputHashMap.containsKey(key)) {
-			Integer tempInt = inputHashMap.get(key);
-			inputHashMap.put(key, tempInt + 1);
-		} else {
-			inputHashMap.put(Long.valueOf(System.currentTimeMillis() / 1000), Integer.valueOf(1));
+	private void writeDataToFile(Long ts) {
+		try {
+			FileWriter fw = new FileWriter(fileName,true);
+			fw.write(ts.toString() + ":"+ eventsSent.toString()+":"+eventsReceived.toString());
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	}
-
-	private synchronized void fileWrite() {
-		Log.info("DataSet over fileWrite called");
-		FileAnalysis obj = new FileAnalysis();
-		// obj.calculateFrequency(inputFile.getAbsolutePath(),
-		// "result-"+pathToFile);
-		obj.calculateFrequencyFromBuffer(inputHashMap, "inputResult.csv");
-		obj.calculateFrequencyFromBuffer(outputHashMap, "outputResult.csv");
-		// obj.calculateFrequency(outFile.getAbsolutePath(),
-		// "result-"+pathToFile);
-		Log.info("experiment over");
 	}
 
 }
