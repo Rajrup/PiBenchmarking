aa41d2b2960a534be8688d5ff9fb0b51e0035b24
shashank <shshnk28@gmail.com>
2016-05-31 13:18:55 +0530

    adds new execution plans

--- src/helperClasses/ExecutionPlan.java
+++ src/helperClasses/ExecutionPlan.java
@@ -36,24 +36,8 @@ public class ExecutionPlan {
                     "from cseEventStream #window.time(1 sec) " + 
                     "select count(height) as countHeight " + 
                     "insert into outputStream ;";
-		}  else if (planType.equals("LenBatAgg")) {
-			executionPlan = "" +
-    				"define stream cseEventStream (height int); " +
-                    "" +
-                    "@info(name = 'query1') " +
-                    "from cseEventStream #window.lengthBatch(5) " + 
-                    "select avg(height) as AvgtHeight " + 
-                    "insert into outputStream ;";
-		} else if (planType.equals("LenBatCount")) {
-			executionPlan = "" +
-    				"define stream cseEventStream (height int); " +
-                    "" +
-                    "@info(name = 'query1') " +
-                    "from cseEventStream #window.lengthBatch(5) " + 
-                    "select count(height) as countHeight " +
-                    "insert into outputStream ;";
-		}
-		else if (planType.equals("pat")) {
+		} 
+		else if (planType.equals("pat3")) {
 			executionPlan = "" +
     				"define stream cseEventStream (height int); " +
                     "" +
@@ -64,32 +48,40 @@ public class ExecutionPlan {
                     "select e1.height as height1, e2.height as height2, e3.height as height3  " + 
                     "insert into outputStream ;";
 			
-		} else if (planType.equals("eqseq")) {
-			executionPlan = "" +
-    				"define stream cseEventStream (height int); " +
-                    "" +
-                    "@info(name = 'query1') " +
-                    "from every e1 =  cseEventStream, e2 = cseEventStream[e1.height == height] " +
-                    "select e1.height as height1, e2.height as height2 " + 
-                    "insert into outputStream ;";
-                    
-		} else if (planType.equals("grtseq")) {
-			executionPlan = "" +
-    				"define stream cseEventStream (height int); " +
-                    "" +
-                    "@info(name = 'query1') " +
-                    "from every e1 =  cseEventStream, e2 = cseEventStream[e1.height > height] " +
-                    "select e1.height as height1, e2.height as height2 " + 
-                    "insert into outputStream ;";
-                    
 		} 
-		else if (planType.equals("seq")) { // for testing
+		
+		else if (planType.equals("pat5")) {
+			executionPlan = "" +
+    				"define stream cseEventStream (height int); " +
+                    "" +
+                    "@info(name = 'query1') " +
+                    "from every e1 =  cseEventStream " +
+                    "-> e2 = cseEventStream[e1.height == e2.height]" +
+                    "-> e3 = cseEventStream[e2.height == e3.height] "+
+                    "-> e4 = cseEventStream[e3.height == e4.height] "+
+                    "-> e5 = cseEventStream[e4.height == e5.height] "+
+                    "-> e6 = cseEventStream[e5.height == e6.height] "+
+                    "select e1.height as height1, e2.height as height2, e3.height as height3, " +
+                    "e4.height as height4, e5.height as height5, e6.height as height6 "+
+                    "insert into outputStream ;";
+			
+		}
+		else if (planType.equals("seq3")) { 
 			executionPlan = "" +
     				"define stream cseEventStream (height int); " +
                     "" +
                     "@info(name = 'query1') " +
                     "from every e1 =  cseEventStream, e2 = cseEventStream[e1.height == e2.height], e3 =  cseEventStream[e3.height == e2.height] " +
                     "select e1.height as height1, e2.height as height2, e3.height as height3 " + 
+                    "insert into outputStream ;";
+		} else if (planType.equals("seq5")) { 
+			executionPlan = "" +
+    				"define stream cseEventStream (height int); " +
+                    "" +
+                    "@info(name = 'query1') " +
+                    "from every e1 =  cseEventStream, e2 = cseEventStream[e1.height == e2.height], e3 =  cseEventStream[e2.height == e3.height], " +
+                    "e4 = cseEventStream[e3.height == e4.height], e5 =  cseEventStream[e4.height == e5.height], e6 =  cseEventStream[e5.height == e6.height] "+
+                    "select e1.height as height1, e2.height as height2, e3.height as height3, e4.height as height4, e5.height as height5, e6.height as height6 " + 
                     "insert into outputStream ;";
 		}
 		return executionPlan;
